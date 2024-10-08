f9b0eaf0de2e1df40d0690462ff4f2194a38a0c8
Dimitar <dstoyanov@MacBook-Pro-4.local>
2024-10-08 17:13:35 +0300

    Initial backend commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.app.Person;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("Gosho", 36);
+        System.out.println(person.name);
+        System.out.println(person.age);
+        System.out.println(person.age);
+    }
+}
---
+++ src/main/java/org/example/app/Person.java
@@ -0,0 +1,18 @@
+package org.example.app;
+
+public class Person {
+    public String name;
+    public int age;
+
+    public Person (String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person (String name){
+        this.name = name;
+    }
+
+    public Person (){
+    }
+}
