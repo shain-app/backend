72ba186848833d68d9d9cbfccf029795b12b655d
Helin Adem <helinadem@Helins-MacBook-Air.local>
2024-10-08 22:55:57 +0300

    Updated project with latest changes

---
+++ src/main/java/org/example/app/Customer.java
@@ -0,0 +1,31 @@
+package org.example.app;
+
+public class Customer {
+
+    private String name;
+    private int age;
+
+    public Customer(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int newAge) {
+        if(newAge > 0) {
+            this.age = newAge;
+        }
+        System.out.println("Age can't be 0 or below 0.");
+    }
+}
---
+++ src/main/java/org/example/app/Item.java
@@ -0,0 +1,48 @@
+package org.example.app;
+
+public class Item {
+
+    private String name;
+    private String size;
+    private double price;
+    private String category;
+
+    public Item(String name, String size, double price, String category) {
+        this.name = name;
+        this.size = size;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
