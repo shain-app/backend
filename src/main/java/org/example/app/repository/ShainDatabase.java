a5955f47557d96965b82da01d70c769917ee729f
Helin Adem <helinadem@Helins-MacBook-Air.local>
2024-10-17 23:57:37 +0300

    Added basic packege structure

---
+++ src/main/java/org/example/app/repository/ShainDatabase.java
@@ -0,0 +1,8 @@
+package org.example.app.repository;
+
+/**
+ * This class will represent an in-memory database. It will have a simple list and pre-initialised items
+ * when the app starts.
+ */
+public class ShainDatabase {
+}
