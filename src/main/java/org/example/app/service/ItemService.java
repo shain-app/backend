21a489f9f3df89566388fa7f3c2accecffd14f2c
Helin Adem <helinadem@Helins-MacBook-Air.local>
2024-11-03 23:47:29 +0200

    Remarks fixed

--- src/main/java/org/example/app/service/ItemService.java
+++ src/main/java/org/example/app/service/ItemService.java
@@ -3,17 +3,11 @@ package org.example.app.service;
 import org.example.app.model.Item;
 import org.example.app.repository.ShainDatabase;
 
-import java.util.List;
-
 public class ItemService {
 
     private final ShainDatabase shainDatabase = new ShainDatabase();
 
     public Item getItem(Integer id) {
         return shainDatabase.getInMemoryDatabase().get(id);
-    }
-
-    public List<Item> getAll() {
-        return shainDatabase.getInMemoryDatabase();
     }
 }
