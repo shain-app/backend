593a86fe8d83a83411a8647111fe3e2bd9e943e6
Helin Adem <helinadem@Helins-MacBook-Air.local>
2024-11-15 19:19:58 +0200

    created customer database

--- src/main/java/org/example/app/service/ItemService.java
+++ src/main/java/org/example/app/service/ItemService.java
@@ -1,32 +1,32 @@
 package org.example.app.service;
 
 import org.example.app.model.Item;
-import org.example.app.repository.ShainDatabase;
+import org.example.app.repository.ItemRepository;
 
 import java.util.List;
 
 public class ItemService {
 
-    private final ShainDatabase shainDatabase = new ShainDatabase();
+    private final ItemRepository itemRepository = new ItemRepository();
 
     public Item getItem(Integer id) {
-        return shainDatabase.getInMemoryDatabase().get(id);
+        return itemRepository.getInMemoryDatabase().get(id);
     }
 
     public List<Item> getAll() {
-        return shainDatabase.getInMemoryDatabase();
+        return itemRepository.getInMemoryDatabase();
     }
 
     // Method to create a new item
     public Item create(Item item) {
-        List<Item> database = shainDatabase.getInMemoryDatabase();
+        List<Item> database = itemRepository.getInMemoryDatabase();
         database.add(item);
         return item;
     }
 
     // Method to remove an item by id
     public void remove(String id) {
-        shainDatabase.getInMemoryDatabase().removeIf(item -> item.getId().equals(id));
+        itemRepository.getInMemoryDatabase().removeIf(item -> item.getId().equals(id));
     }
 }
 
